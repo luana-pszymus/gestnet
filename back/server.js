9b375b6fca106f76bb42ddd4c8533ba6df831a6a
luana-pszymus <ens-luanapszymus@ugv.edu.br>
2026-05-05 16:27:07 -0300

    alter

--- back/server.js
+++ back/server.js
@@ -2,4 +2,7 @@ require("dotenv").config();
 const app = require("./src/app");
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Servidor rodando na porta ${PORT}`),
+);
